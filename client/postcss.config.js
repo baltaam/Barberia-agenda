7787eef1a86710550c5b3da2705c35550c1dc44e
Baltazar Mendoza <mendozabaltazar11@gmail.com>
2026-03-03 01:54:10 -0300

    Forzando estilos

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 }
