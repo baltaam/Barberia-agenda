369d23dceb05a8529f06bd764081684a67828e50
Baltazar Mendoza <mendozabaltazar11@gmail.com>
2026-03-03 02:01:00 -0300

    Fix: Ajuste de rutas y config de Tailwind para producción

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Esto asegura que las rutas de los estilos siempre empiecen desde la raíz
 })
