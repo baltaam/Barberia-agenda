d8caabf0ad3825d329004954bdc4341f138fc4c8
Baltazar Mendoza <mendozabaltazar11@gmail.com>
2026-03-03 11:39:03 -0300

    Fix final: Tailwind v4 funcionando

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,13 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: {
+    "@tailwindcss/postcss": {},
+    "autoprefixer": {},
+  }
 }
